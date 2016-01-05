d077c981ea8cfbfdc6ac29aec3fb30f2a63f6b29
Max Claus Nunes <maxcnunes@gmail.com>
2016-01-05 20:39:19 -0200

    Use milliseconds instead of seconds
    The challenge asks for milliseconds and actually is funnier this way because is harder to match the GO time.

--- lib/user_reflexes.ex
+++ lib/user_reflexes.ex
@@ -3,10 +3,10 @@ defmodule UserReflexes do
   @max_end_time 10
 
   def run do
-    random_time = :random.uniform(@max_start_time)
-    IO.puts("Press ENTER in #{random_time} seconds")
+    random_time = :random.uniform(@max_start_time) * 1000
+    IO.puts("Press ENTER in #{random_time} milliseconds")
 
-    current_time = Timex.Time.now(:secs)
+    current_time = Timex.Time.now(:msecs)
     go_time = current_time + random_time
 
     pid_timer = show_timer(go_time)
@@ -14,13 +14,13 @@ defmodule UserReflexes do
 
     receive do
       {:ok, answered_at}  ->
-        current_time = Timex.Time.now(:secs)
+        current_time = Timex.Time.now(:msecs)
         cond do
           answered_at < go_time -> IO.puts("FAILED: Answred before the GO time")
           answered_at > go_time -> IO.puts("FAILED: Answred after the GO time")
           true -> IO.puts("SUCCESS: Perfect!")
         end
-        IO.puts("Answred at #{current_time - go_time} secs")
+        IO.puts("Answred at #{current_time - go_time} milliseconds")
 
         # leave children process
         Process.exit(pid_answer, :exit)
@@ -34,12 +34,12 @@ defmodule UserReflexes do
     caller = self()
     spawn(fn ->
       IO.gets("")
-      send(caller, {:ok, Timex.Time.now(:secs) })
+      send(caller, {:ok, Timex.Time.now(:msecs) })
     end)
   end
 
   defp show_timer(go_time) do
-    current_time = Timex.Time.now(:secs)
+    current_time = Timex.Time.now(:msecs)
     remaining_time = go_time - current_time
     spawn(fn -> show_timer(go_time, remaining_time) end)
   end
@@ -49,12 +49,13 @@ defmodule UserReflexes do
   end
 
   defp show_timer(go_time, remaining_time) do
-    current_time = Timex.Time.now(:secs)
+    current_time = Timex.Time.now(:msecs)
     rt = go_time - current_time
+    rt_secs = div(rt, 1_000)
 
     # only prints again if the second has changed
-    if (rt != remaining_time) do
-      IO.puts("> #{rt}")
+    if (rt_secs != div(remaining_time, 1_000)) do
+      IO.puts("> #{rt_secs}")
     end
 
     show_timer(go_time, rt)
